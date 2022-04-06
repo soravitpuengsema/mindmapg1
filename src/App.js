dd6ad235e7eb6b29e4f3cd4d8de88377dc79fe43
soravitpuengsema <s6301012630184@email.kmutnb.ac.th>
2022-04-06 23:47:16 +0700

    mindmap and save as img, json

--- src/App.js
+++ src/App.js
@@ -1,24 +1,92 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect} from 'react';
+import ReactDOM from "react-dom";
+import MindElixir, { E } from "mind-elixir";
+//import {writeJsonFile} from 'write-json-file';
+import painter from 'mind-elixir/dist/painter';
+
+var a = '';
+var mindtemp = null;
 
 function App() {
+
+  const [update, setUpdate] = useState();
+
+  let options = {
+    el: "#map",
+    direction: MindElixir.LEFT,
+    data: MindElixir.new("new topic"),
+    draggable: true, // default true
+    contextMenu: true, // default true
+    toolBar: true, // default true
+    nodeMenu: true, // default true
+    keypress: true // default true
+  }
+
+  //let mind = new MindElixir(options);
+
+  useEffect(() => {
+
+    let mind = new MindElixir(options);
+
+    //setMind(mind);
+    mind.init();
+
+    mind.getAllDataString();
+
+    // get a node
+    //console.log(mind.getAllDataMd());
+    //dataUpdate(mind.getAllData());
+
+    mind.bus.addListener('operation', operation => {
+
+      //console.log(mind.getAllDataString());
+      a = mind.getAllDataString();
+
+      //painter.exportPng(mind,'picture')
+
+      mindtemp = mind;
+      
+      // return {
+      //   name: action name,
+      //   obj: target object
+      // }
+
+      // name: [insertSibling|addChild|removeNode|beginEdit|finishEdit]
+      // obj: target
+
+      // name: moveNode
+      // obj: {from:target1,to:target2}
+
+    })
+  });
+
+  const exportData = () => {
+    const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
+      a
+    )}`;
+    const link = document.createElement("a");
+    link.href = jsonString;
+    link.download = "data.json";
+
+    link.click();
+  };
+
+  const paint = () => {
+    //console.log(mind.getAllDataMd());
+    console.log(a);
+    painter.exportPng(mindtemp,'picture');
+    //const fs = require('fs');
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <div id="map" style={{ height: "500px", width: "100%" }} />
+    <button onClick={paint}>Save as Image</button>
+    <button onClick={exportData}>Save as JSON</button>
+  </>
   );
 }
 
